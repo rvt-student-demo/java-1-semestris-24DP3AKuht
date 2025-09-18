d80c4dd57fce6e230848ed38f7064a80a3b91a69
Aleksandrs_Kuhtarskis <A240317AK@rvt.lv>
2025-09-18 14:26:27 +0300

    Average of three numbers

---
+++ src/main/java/rvt/average.java
@@ -0,0 +1,15 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class average {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int a = 3, b = 7, c = 17; 
+
+        double avg = (a + b + c) / 3.0; 
+        System.out.println("Average: " + avg);
+    }
+}
