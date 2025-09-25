e79953f637e026943207e4e691880e873c644131
Aleksandrs_Kuhtarskis <A240317AK@rvt.lv>
2025-09-25 15:26:24 +0300

    delikates

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,29 @@
 package rvt;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+        Scanner scanner = new Scanner(System.in)
+
+        System.out.print("Give ohe first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Give the second number: ");
+        int num2 = scanner.nextInt();
+
+        int summa = num1 + num2;
+
+        int starpiba = num1 - num2;
+
+        int reizinasana = num1 * num2;
+
+        double dalisana = 1.0 * num1 / num2;
+        
+        System.out.println(summa);
+        System.out.println(starpiba);
+        System.out.println(reizinasana);
+        System.out.println(dalisana);
+
     }
 }
