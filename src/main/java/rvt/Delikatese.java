15cc6b86364018572e9b5542b1e4926c97afb012
Aleksandrs_Kuhtarskis <A240317AK@rvt.lv>
2025-12-01 09:08:12 +0200

    delikatese uzdevums

---
+++ src/main/java/rvt/Delikatese.java
@@ -0,0 +1,23 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class delikatese {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ievadi preci: ");
+        String prece = scanner.nextLine();
+
+        System.out.print("Ievdi cenu: ");
+        int cena = scanner.nextLine();
+
+        System.out.print("Ekspress piegade (0==ne, 1==ja):");
+        int ekspress = scanner.nextInt();
+
+        if(cena < 10);
+
+    }
+    
+    
+}
