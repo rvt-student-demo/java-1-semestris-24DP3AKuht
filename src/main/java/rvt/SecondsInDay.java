b6a15e9fc6e895f17840a1af82bf3d32cec47809
Aleksandrs_Kuhtarskis <A240317AK@rvt.lv>
2025-12-04 11:59:40 +0200

    Programma kas izskaita cik sekondus dienā

---
+++ src/main/java/rvt/SecondsInDay.java
@@ -0,0 +1,17 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class SecondsInDay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ievadiet dienu skaitu: ")
+        int days = scanner.nextInt();
+
+        int seconds = days * 24 * 60 * 60;
+
+        System.out.println("In " + days + "dienās" + seconds + " sekundes");
+    }
+    
+}
